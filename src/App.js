a397ac14bd02256b2d1d2f80eaaec194a5c638e1
Lorenzzz90 <lorenzomarziali90@gmail.com>
2019-06-26 12:30:27 +0200

    prova

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          General Kenobi!!!!!
+          General Kenobi!!
         </p>
         <a
           className="App-link"
